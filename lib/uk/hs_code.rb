611cd10ef96527ea2b7c3abcb55d31415dc66f4d
Mateusz Michalski <enigmatt.priv@gmail.com>
2023-04-23 13:36:47 +0100

    Initial commit

---
+++ lib/uk/hs_code.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "hs_code/version"
+
+module Uk
+  module HsCode
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/uk/hs_code_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Uk::HsCode do
+  it "has a version number" do
+    expect(Uk::HsCode::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
